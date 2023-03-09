cc663f27896a7f7c86dfa0713ad6dd1d440bed30
SaaberDev <saaberdev@gmail.com>
2023-03-09 05:02:54 +0100

    - added functionalities for find the maximum number

---
+++ ConsoleEx2/ControlFlow/FindMaximumNumber.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleEx2.ControlFlow;
+
+public class FindMaximumNumber
+{
+    public void GetMaxNumber()
+    {
+        AskQuestion:
+        Console.WriteLine("Please enter a series of numbers separated by comma.");
+        
+        var input = Console.ReadLine();
+        
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            var isMatched = Regex.IsMatch(input, "^([0-9]*)+(,[0-9]+)+$");
+            if (!isMatched)
+            {
+                Console.WriteLine("You probably forgot to add comma or ended with a comma. Please check and try again. i.e. 1,2,3");
+                goto AskQuestion;
+            }
+
+            var inputAsArray = input.Split(",");
+                
+            // Way - 1: Convert string array to int array and directly grab max value from array
+            // var numbers = Array.ConvertAll(inputAsArray, int.Parse);
+            // var max = numbers.Max();
+                
+            // Way - 2: use loop to find max value
+            var max = Convert.ToInt32(inputAsArray[0]);
+            foreach (var value in inputAsArray)
+            {
+                var number = Convert.ToInt32(value);
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine($"The max number is {max}");
+        }
+        else
+        {
+            Console.WriteLine("You did not provide any numbers !!");
+            goto AskQuestion;
+        }
+    }
+}
--- ConsoleEx2/Program.cs
+++ ConsoleEx2/Program.cs
@@ -1,5 +1,12 @@
 ﻿using ConsoleEx2.ControlFlow;
 
+Console.Write(@"1. Count Division By Number
+2. Sum Of Previously Entered Numbers
+3. Compute Factorial Number
+4. Guess The Number
+5. Find The Maximum Number
+");
+Console.WriteLine("=============================");
 Console.WriteLine("Please choose a program between 1 and 5.");
 
 var program = Convert.ToInt16(Console.ReadLine());
@@ -7,23 +14,43 @@ var program = Convert.ToInt16(Console.ReadLine());
 switch (program)
 {
     case 1:
+        Console.WriteLine("=============================");
+        Console.WriteLine("Program 1: Count Division By Number");
+        // ask user a number and count how many numbers between 1 and 100 are divisible by the given number
         (new CountDivisionByNumber()).CountDividable();
     break;
     
     case 2:
+        Console.WriteLine("=============================");
+        Console.WriteLine("Program 2: Sum of Previously Entered Numbers");
+        // continuously ask the user to enter a number, when done type "ok" to exit.
+        // calculate the sum of all the previously entered numbers and display it on the console.
         (new SumOfPreviousNumber()).SumPreviousNumbers();
     break;
     
     case 3:
+        Console.WriteLine("=============================");
+        // ask the user to enter a number.
+        // compute the factorial of the given number.
+        Console.WriteLine("Program 3: Compute Factorial Number");
         (new ComputeFactorial()).GetFactorial();
     break;
     
     case 4:
+        Console.WriteLine("=============================");
+        Console.WriteLine("Program 4: Guess The Number");
+        // user need to guess a number to win the game, if user guess wrong number user will lose.
+        // user will get 4 chances to guess the number.
+        // note that, for experiment testing purpose random generated number will be displayed in the console.
         (new GuessTheNumber()).PickARandomNumber();
         break;
     
     case 5:
-        // code goes here
+        Console.WriteLine("=============================");
+        Console.WriteLine("Program 5: Find The Maximum Number");
+        // ask the user to enter a series of numbers separated by comma.
+        // program will find the maximum number from the series of numbers.
+        (new FindMaximumNumber()).GetMaxNumber();
         break;
     
     default:
